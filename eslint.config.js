5831e5dce3bb532b4bacf7ca9cb1637913c898fe
Kodeneko <ladysun.freedom@gmail.com>
2024-07-23 17:24:52 +0200

    [master] reduce eslint code config

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,5 @@
-import pluginJs from '@eslint/js';
-import stylisticJs from '@stylistic/eslint-plugin-js';
-import stylisticJsx from '@stylistic/eslint-plugin-jsx';
-import stylisticTs from '@stylistic/eslint-plugin-ts';
+import eslintJs from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
 import parserTs from '@typescript-eslint/parser';
 import pluginReact from 'eslint-plugin-react';
 import pluginReactHooks from 'eslint-plugin-react-hooks';
@@ -9,22 +7,21 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import sortKeys from 'eslint-plugin-sort-keys';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
-// Import pluginPrettierRec from "eslint-plugin-prettier/recommended";
-
+import eslintTs from 'typescript-eslint';
 
 export default [
 
 
-  // Javascript
+  // Javascript & Typescript
   {
-    files: ['**/*.{js,jsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     plugins: {
       'simple-import-sort': simpleImportSort,
       'sort-keys': sortKeys
     },
     rules: {
-      ...pluginJs.configs.recommended.rules,
+      ...eslintJs.configs.recommended.rules,
+      ...eslintTs.configs.recommended.rules,
       'simple-import-sort/exports': 'error',
       'simple-import-sort/imports': 'error',
       'sort-imports': 'off',
@@ -33,110 +30,36 @@ export default [
     }
   },
 
-  // Typescript
+  // Stylistic
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{js,jsx, ts, tsx}'],
     plugins: {
-      'simple-import-sort': simpleImportSort,
-      'sort-keys': sortKeys
+      '@stylistic': stylistic
     },
     rules: {
-      ...tseslint.configs.recommended.rules,
-      'simple-import-sort/exports': 'error',
-      'simple-import-sort/imports': 'error',
-      'sort-imports': 'off',
-      'sort-keys': 'error',
-      'sort-keys/sort-keys-fix': 'error'
-    }
-  },
-
-  // Stylistic js
-  {
-    files: ['**/*.{js,jsx}'],
-    plugins: {
-      '@stylistic/js': stylisticJs
-    },
-    rules: {
-      ...stylisticJs.configs['all-flat'].rules,
-      '@stylistic/js/indent': [
+      ...stylistic.configs['all-flat'].rules,
+      '@stylistic/indent': [
         'error',
         2
       ],
-      '@stylistic/js/quote-props': [
+      '@stylistic/linebreak-style': [
+        'error',
+        'unix'
+      ],
+      '@stylistic/quote-props': [
         'error',
         'as-needed'
       ],
-      '@stylistic/js/quotes': [
+      '@stylistic/quotes': [
         'error',
         'single'
       ]
     }
   },
-
-  // Stylistic ts
-  {
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      parser: parserTs
-    },
-    plugins: {
-      '@stylistic/ts': stylisticTs
-    },
-    rules: {
-      ...stylisticTs.configs['all-flat'].rules,
-      '@stylistic/ts/indent': [
-        'error',
-        2
-      ],
-      '@stylistic/ts/quote-props': [
-        'error',
-        'as-needed'
-      ],
-      '@stylistic/ts/quotes': [
-        'error',
-        'single'
-      ]
-    }
-
-  },
-
-  // Stylistic jsx
-  {
-    ...stylisticJsx.configs['all-flat'],
-    rules: {
-      '@stylistic/jsx/jsx-indent': [
-        2,
-        2
-      ],
-      '@stylistic/jsx/jsx-indent-props': [
-        2,
-        2
-      ],
-      '@stylistic/jsx/jsx-max-props-per-line': [
-        1,
-        {maximum: 2}
-      ],
-      '@stylistic/jsx/jsx-one-expression-per-line': [
-        1,
-        {allow: 'literal'}
-      ]
-    }
-  },
-
-  /*
-   * // Prettier
-   *  {
-   *    ...pluginPrettierRec,
-   *    rules: {
-   *
-   *    },
-   *  },
-   */
 
   // React
   {
-    ...pluginReact.configs.flat.all,
-    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
+    files: ['**/*.{js,jsx,ts,tsx}'],
     languageOptions: {
       globals: {
         ...globals.browser
@@ -148,30 +71,26 @@ export default [
         }
       }
     },
-    rules: {
-      'react/react-in-jsx-scope': 'off'
-    }
-  },
-
-
-  // React Hooks
-  {
     plugins: {
-      'react-hooks': pluginReactHooks
-    },
-    rules: {
-      'react-hooks/exhaustive-deps': 'warn',
-      'react-hooks/rules-of-hooks': 'error'
-    }
-  },
-
-  // React Refresh
-  {
-    plugins: {
+      react: pluginReact,
+      'react-hooks': pluginReactHooks,
       'react-refresh': reactRefresh
     },
     rules: {
-      'react-refresh/only-export-components': 'warn'
+      'react-hooks/exhaustive-deps': 'warn',
+      'react-hooks/rules-of-hooks': 'error',
+      'react-refresh/only-export-components': 'warn',
+      'react/react-in-jsx-scope': 'off'
     }
   }
+
+  /*
+   * // Prettier
+   *  {
+   *    ...pluginPrettierRec,
+   *    rules: {
+   *
+   *    },
+   *  },
+   */
 ];
