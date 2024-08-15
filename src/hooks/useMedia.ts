41ac0e552d719e428463b2c3f6ff745c2524984c
Kodeneko <ladysun.freedom@gmail.com>
2024-08-15 19:30:01 +0200

    [responsive] fix hook screen detector

--- src/hooks/useMedia.ts
+++ src/hooks/useMedia.ts
@@ -2,8 +2,8 @@ import { mediaMobile, mediaTablet, mediaWeb } from '@globals/media';
 import { useState } from 'react';
 
 function useMedia (size: string) {
-  const [ sw, setSw ] = useState<boolean>(false);
   const mediaQueryList = window.matchMedia(`(max-width: ${size})`);
+  const [ sw, setSw ] = useState<boolean>(mediaQueryList.matches);
   mediaQueryList.addEventListener('change', (event) => {
     setSw(event.matches);
   });
