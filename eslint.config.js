50ad4ffbcf4ced04071eef0fa5a93a2f0b17ce91
Kodeneko <ladysun.freedom@gmail.com>
2025-05-19 00:14:53 +0200

    [dev] eslint order import

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,17 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
-import pluginReact from 'eslint-plugin-react'
-import { defineConfig } from 'eslint/config'
-import stylistic from '@stylistic/eslint-plugin'
-import reactHooks from 'eslint-plugin-react-hooks'
+import js from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import { defineConfig } from 'eslint/config';
+import importPlugin from 'eslint-plugin-import';
+import pluginReact from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig([
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
   { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], languageOptions: { globals: globals.browser } },
+  importPlugin.flatConfigs.recommended,
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   stylistic.configs.recommended,
@@ -17,7 +20,10 @@ export default defineConfig([
     files: ['**/*.{jsx,tsx}'],
     settings: { react: { version: 'detect' } } },
   { rules: {
+    'import/no-unresolved': 'off',
     'react/react-in-jsx-scope': 'off',
+    'import/no-duplicates': 'error',
+    '@stylistic/semi': ['error', 'always'],
     '@stylistic/indent': ['error', 2],
     '@stylistic/jsx-indent': ['error', 2],
     // '@stylistic/curly-newline': ['error', { minElements: 4 }],
@@ -32,5 +38,8 @@ export default defineConfig([
     '@stylistic/comma-spacing': ['error', { before: false, after: true }],
     '@stylistic/dot-location': ['error', 'property'],
     '@stylistic/multiline-ternary': ['error', 'always-multiline']
-  } }
-])
+  } },
+  { plugins: { 'simple-import-sort': simpleImportSort }, rules: { 'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+    'import/first': 'off' } }
+]);
--- src/components/badge-icon/BadgeIcon.tsx
+++ src/components/badge-icon/BadgeIcon.tsx
@@ -1,24 +1,24 @@
-import { IconName } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { FormType, SizeType } from '@models/app'
-import clsx from 'clsx'
+import type { IconProp } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FormType, SizeType } from '@models/app';
+import clsx from 'clsx';
 
-import styles from './style.module.less'
-import BadgeIconProps from './type'
+import styles from './style.module.less';
+import BadgeIconProps from './type';
 
 const BadgeIcon = ({
   shape = FormType.CIRCLE, invert = false, size = SizeType.MD, icon
 }: BadgeIconProps) => {
-  const styleWrap = clsx(styles.wrap, styles[size], styles[shape], styles[invert ? 'invert' : 'reg'])
+  const styleWrap = clsx(styles.wrap, styles[size], styles[shape], styles[invert ? 'invert' : 'reg']);
 
   return (
     <div className={styleWrap}>
       <FontAwesomeIcon
         className={styles.icon}
-        icon={icon as IconName}
+        icon={icon as IconProp}
       />
     </div>
-  )
-}
+  );
+};
 
-export default BadgeIcon
+export default BadgeIcon;
