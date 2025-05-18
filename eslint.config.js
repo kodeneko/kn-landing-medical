24b00a7e0baaf354143fabc2cc5213d53840c51f
Kodeneko <ladysun.freedom@gmail.com>
2025-05-18 23:08:12 +0200

    [dev] Changes eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,140 +1,36 @@
-import eslintJs from '@eslint/js';
-import stylistic from '@stylistic/eslint-plugin';
-import parserTs from '@typescript-eslint/parser';
-import jsonc from 'eslint-plugin-jsonc';
-import jsxA11y from 'eslint-plugin-jsx-a11y';
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import simpleImportSort from 'eslint-plugin-simple-import-sort';
-import sortKeys from 'eslint-plugin-sort-keys';
-import storybook from 'eslint-plugin-storybook';
-import globals from 'globals';
-import parserJsonc from 'jsonc-eslint-parser';
-import eslintTs from 'typescript-eslint';
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import pluginReact from 'eslint-plugin-react'
+import { defineConfig } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
+import reactHooks from 'eslint-plugin-react-hooks'
 
-storybook.configs;
-
-export default [
-
-  // Json - Jsonc - Json5
-  {
-    files: [ '**/*.{json,jsonc}' ],
-    languageOptions: { parser: parserJsonc },
-    plugins: { jsonc },
-    rules: {
-      ...jsonc.configs['flat/all'].rules,
-      'jsonc/indent': [
-        'error',
-        2,
-        {}
-      ]
-    }
-  },
-
-  // Javascript & Typescript
-  {
-    files: [ '**/*.{js,jsx,ts,tsx}' ],
-    plugins: {
-      'simple-import-sort': simpleImportSort,
-      'sort-keys': sortKeys
-    },
-    rules: {
-      ...eslintJs.configs.recommended.rules,
-      ...eslintTs.configs.recommended.rules,
-      'no-unused-vars': 'off',
-      'simple-import-sort/exports': 'error',
-      'simple-import-sort/imports': 'error',
-      'sort-imports': 'off',
-      'sort-keys': 'error',
-      'sort-keys/sort-keys-fix': 'error'
-    }
-  },
-
-  // Node config files
-  {
-    files: [ '**/vite.config.ts' ],
-    languageOptions: {
-      globals: { ...globals.node },
-      parser: parserTs
-    }
-  },
-
-  // Stylistic
-  {
-    files: [ '**/*.{js,jsx,ts,tsx}' ],
-    plugins: { '@stylistic': stylistic },
-    rules: {
-      ...stylistic.configs['all-flat'].rules,
-      '@stylistic/array-bracket-spacing': [ 'error', 'always' ],
-      '@stylistic/array-element-newline': [ 'error', { minItems: 3 } ],
-      '@stylistic/brace-style': [
-        'error',
-        '1tbs',
-        { allowSingleLine: true }
-      ],
-      '@stylistic/comma-spacing': [
-        'error', {
-          after: true,
-          before: false
-        }
-      ],
-      '@stylistic/dot-location': [ 'error', 'property' ],
-      '@stylistic/function-call-argument-newline': [ 'error', 'consistent' ],
-      '@stylistic/indent': [ 'error', 2 ],
-      '@stylistic/jsx-closing-tag-location': 'error',
-      '@stylistic/jsx-max-props-per-line': [ 1, { maximum: 2 } ],
-      '@stylistic/linebreak-style': [ 'error', 'unix' ],
-      '@stylistic/no-multi-spaces': 'error',
-      '@stylistic/no-multiple-empty-lines': [
-        'error', {
-          max: 1,
-          maxBOF: 1
-        }
-      ],
-      '@stylistic/object-curly-newline': [ 'error', { multiline: true } ],
-      '@stylistic/object-curly-spacing': [ 'error', 'always' ],
-      '@stylistic/padded-blocks': [
-        'error', {
-          blocks: 'never',
-          classes: 'always'
-        }
-      ],
-      '@stylistic/quote-props': [ 'error', 'as-needed' ],
-      '@stylistic/quotes': [ 'error', 'single' ]
-    }
-  },
-
-  // React
-  {
-    files: [ '**/*.{js,jsx,ts,tsx}' ],
-    languageOptions: {
-      globals: { ...globals.browser },
-      parser: parserTs,
-      parserOptions: { ecmaFeatures: { jsx: true } }
-    },
-    plugins: {
-      // 'jsx-a11y': jsxA11y,
-      react: react,
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh
-    },
-    rules: {
-      ...react.configs.recommended.rules,
-      // ...jsxA11y.flatConfigs.recommended.rules,
-      'react-hooks/exhaustive-deps': 'warn',
-      'react-hooks/rules-of-hooks': 'error',
-      'react-refresh/only-export-components': 'warn',
-      'react/jsx-closing-tag-location': [ 1, { location: 'tag-aligned' } ],
-      'react/react-in-jsx-scope': 'off'
-    }
-  },
-
-  // Storybook
-  {
-    files: [ 'src/stories/**/*.story.ts' ],
-    plugins: { storybook },
-    rules: { ...storybook.configs.recommended.rules }
-  }
-
-];
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], languageOptions: { globals: globals.browser } },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  stylistic.configs.recommended,
+  reactHooks.configs['recommended-latest'],
+  { plugins: { pluginReact },
+    files: ['**/*.{jsx,tsx}'],
+    settings: { react: { version: 'detect' } } },
+  { rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@stylistic/indent': ['error', 2],
+    '@stylistic/jsx-indent': ['error', 2],
+    // '@stylistic/curly-newline': ['error', { minElements: 4 }],
+    '@stylistic/array-element-newline': ['error', { minItems: 4 }],
+    '@stylistic/array-bracket-newline': ['error', { minItems: 4 }],
+    '@stylistic/function-call-argument-newline': ['error', 'consistent'],
+    '@stylistic/newline-per-chained-call': ['error', { ignoreChainWithDepth: 2 }],
+    '@stylistic/object-curly-newline': ['error', { minProperties: 4 }],
+    '@stylistic/operator-linebreak': ['error', 'before'],
+    '@stylistic/arrow-parens': ['error', 'as-needed'],
+    '@stylistic/comma-dangle': ['error', 'never'],
+    '@stylistic/comma-spacing': ['error', { before: false, after: true }],
+    '@stylistic/dot-location': ['error', 'property'],
+    '@stylistic/multiline-ternary': ['error', 'always-multiline']
+  } }
+])
--- src/components/badge-icon/BadgeIcon.tsx
+++ src/components/badge-icon/BadgeIcon.tsx
@@ -1,15 +1,15 @@
-import { IconName } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { FormType, SizeType } from '@models/app';
-import clsx from 'clsx';
+import { IconName } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { FormType, SizeType } from '@models/app'
+import clsx from 'clsx'
 
-import styles from './style.module.less';
-import BadgeIconProps from './type';
+import styles from './style.module.less'
+import BadgeIconProps from './type'
 
-const BadgeIcon = ({ shape = FormType.CIRCLE, invert = false, size = SizeType.MD, icon }: BadgeIconProps) => {
-  const styleWrap = clsx(styles.wrap, styles[size], styles[shape], styles[invert
-    ? 'invert'
-    : 'reg']);
+const BadgeIcon = ({
+  shape = FormType.CIRCLE, invert = false, size = SizeType.MD, icon
+}: BadgeIconProps) => {
+  const styleWrap = clsx(styles.wrap, styles[size], styles[shape], styles[invert ? 'invert' : 'reg'])
 
   return (
     <div className={styleWrap}>
@@ -18,7 +18,7 @@ const BadgeIcon = ({ shape = FormType.CIRCLE, invert = false, size = SizeType.MD
         icon={icon as IconName}
       />
     </div>
-  );
-};
+  )
+}
 
-export default BadgeIcon;
+export default BadgeIcon
